be6e73edb6d5fc1a86be9c9476b81264a69e3a5d
Eric <ercdavgn@gmail.com>
2016-12-27 13:10:06 -0500

    Delete page_control.js

--- inspection_app/js/page_control.js
+++
@@ -1,7 +0,0 @@
-if(document.URL.indexOf("index.html")>=0){
-    $('#includeMain').css({padding:0})
-}
-$(function() {
-    var appBtnWidth = $('.app-btn').width();
-    $('.app-btn').height(appBtnWidth);
-});
