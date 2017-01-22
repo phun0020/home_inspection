9db7e8ed312d1622449015aca57998e3fdbffd85
Eric <ercdavgn@gmail.com>
2017-01-22 11:10:01 -0500

    Add files via upload

--- IAWD/urls.py
+++ IAWD/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^inspection/', include('home_inspection.urls')),
+    url(r'^', include('home_inspection.urls')),
 ]
