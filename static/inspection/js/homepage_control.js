be46c7943b018953a7928a9de8458f42da4f0eaa
Eric <ercdavgn@gmail.com>
2017-02-18 15:53:04 -0500

    Add files via upload

---
+++ static/inspection/js/homepage_control.js
@@ -0,0 +1,5 @@
+// remove padding on home page load
+var pagewidth = $("body").innerWidth();
+    if(pagewidth>=768){
+    $('#includeMain').css({padding:0})
+    }
---
+++ static/inspection/js/index.js
@@ -0,0 +1,35 @@
+var index = (function () {
+    // call ajax when add new property
+    var ajaxAdd = function () {
+        $.ajax({
+            url: '/inspection/',
+            type: 'POST',
+            data: $('#newPropForm').serialize(),
+            success: function (response, textStatus, jqXHR) {
+                // debugger;
+                $('#property-container').html(response);
+                $(':input','#newPropForm')
+                .not(':button, :submit, :reset, :hidden')
+                .val('')
+                .removeAttr('checked')
+                .removeAttr('selected');
+            }
+        });
+    };
+
+    var ajaxDelete = function (id) {
+        $.ajax({
+            url: '/dashboard/' + id + '/delete',
+            type: 'POST',
+            success: function (response, textStatus, jqXHR) {
+                $('#property-container').html(response);
+            }
+        });
+    };
+
+    return {
+        ajaxAdd: ajaxAdd,
+        ajaxDelete : ajaxDelete
+    };
+
+})();
